8ad89257f5908eea45299313b237385fb88c7c16
rzvpoi <65354895+rzvpoi@users.noreply.github.com>
2022-12-11 10:33:59 +0200

    First Upload

---
+++ models/user.go
@@ -0,0 +1,90 @@
+package models
+
+import (
+	"errors"
+	"flashStudyAPI/utils/token"
+	"html"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	gorm.Model
+	Email    string `gorm:"size:255;not null;unique" json:"email"`
+	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Password string `gorm:"size:255;not nulll" json:"password"`
+}
+
+func GetUserByID(uid uint) (User, error) {
+
+	var u User
+
+	if err := DB.First(&u, uid).Error; err != nil {
+		return u, errors.New("User not found")
+	}
+
+	u.PrepareGive()
+
+	return u, nil
+}
+
+func (u *User) PrepareGive() {
+	u.Password = ""
+}
+
+func VerifyPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func LoginCheck(email, password string) (string, error) {
+	var err error
+
+	u := User{}
+
+	err = DB.Model(User{}).Where("email = ?", email).Take(&u).Error
+
+	if err != nil {
+		return "", err
+	}
+
+	err = VerifyPassword(password, u.Password)
+
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return "", err
+	}
+
+	token, err := token.GenerateToken(u.ID)
+
+	return token, nil
+}
+
+func (u *User) SaveUser() (*User, error) {
+	var err error
+	err = DB.Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+
+	return u, nil
+}
+
+func (u *User) BeforeSave() error {
+
+	//turn password into hash
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+
+	//remove spaces in username
+	u.Username = html.EscapeString(strings.TrimSpace(u.Username))
+
+	//remove spaces in username
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+
+	return nil
+
+}
